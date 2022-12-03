f009e1f8b37dd1ec86674efa15d43bd84683825c
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-12-03 15:13:24 +0100

    Cleaning

--- scoring_runage.py
+++ scoring_runage.py
@@ -26,87 +26,98 @@ pd.options.mode.chained_assignment = None  # default='warn'
 category_selected = ['Violent', 'Will', 'Destroy', 'Despair']
 category_value = ", ".join(category_selected)
 
-coef_set = {'Violent' : 3,
-            'Will' : 3,
-            'Destroy' : 2,
-            'Despair' : 2}
+coef_set = {'Violent': 3,
+            'Will': 3,
+            'Destroy': 2,
+            'Despair': 2}
 
 category_selected_spd = ['Violent', 'Will', 'Destroy', 'Despair', 'Swift']
 category_value_spd = ", ".join(category_selected)
 
-coef_set_spd = {'Violent' : 3,
-            'Will' : 3,
-            'Destroy' : 2,
-            'Despair' : 2,
-            'Swift' : 3}
+coef_set_spd = {'Violent': 3,
+                'Will': 3,
+                'Destroy': 2,
+                'Despair': 2,
+                'Swift': 3}
 
 # CSS
 
-st.markdown("<h1 style='text-align: center; color: white;'>Scoring SW </h1>", unsafe_allow_html=True )
+st.markdown("<h1 style='text-align: center; color: white;'>Scoring SW </h1>",
+            unsafe_allow_html=True)
 
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
+
 
 # import streamlit upload st.session_state.file (data)
 
-if 'submitted' not in st.session_state: # si submitted n'est pas initialisé, c'est que le joueur vient d'arriver et n'a pas upload de json. C'est donc false.
+# si submitted n'est pas initialisé, c'est que le joueur vient d'arriver et n'a pas upload de json. C'est donc false.
+if 'submitted' not in st.session_state:
     st.session_state.submitted = False
 
-if st.session_state['submitted'] == False: # PAs de json initialisé. Donc menu avec Upload json
+# PAs de json initialisé. Donc menu avec Upload json
+if st.session_state['submitted'] == False:
     menu_selected = ['Upload JSON', 'General', 'Evolution']
     icons_selected = ["gear", 'info', 'kanban']
-else: # Json upload, la première page n'est plus utile
-    if 'guildeid' in st.session_state: # Si on a l'info sur la guilde
+else:  # Json upload, la première page n'est plus utile
+    if 'guildeid' in st.session_state:  # Si on a l'info sur la guilde
         # if st.session_state['guildeid'] == 116424:  # si c'est l'id d'Endless, on peut ouvrir le suivi
-        if st.session_state['pseudo'] == 'Tømløra': # si c'est l'admin
-            menu_selected = ['General', 'Evolution', 'Classement', 'Suivi', 'Runes', 'Bestiaire', 'Parametres']
-            icons_selected = ["info", 'kanban', 'kanban', 'bag-check-fill', 'book', 'gear']
-        else: # si c'est pas l'admin
-            menu_selected = ['General', 'Evolution', 'Classement', 'Runes', 'Bestiaire', 'Parametres']
-            icons_selected = ["info", 'kanban', 'ladder', 'bag-check-fill', 'book', 'gear']
-    else: # si on a pas l'info sur la guilde
-        menu_selected = ['General', 'Evolution', 'Classement', 'Runes', 'Parametres']
+        if st.session_state['pseudo'] == 'Tømløra':  # si c'est l'admin
+            menu_selected = ['General', 'Evolution', 'Classement',
+                             'Suivi', 'Runes', 'Bestiaire', 'Parametres']
+            icons_selected = ["info", 'kanban', 'kanban',
+                              'bag-check-fill', 'book', 'gear']
+        else:  # si c'est pas l'admin
+            menu_selected = ['General', 'Evolution',
+                             'Classement', 'Runes', 'Bestiaire', 'Parametres']
+            icons_selected = ["info", 'kanban', 'ladder',
+                              'bag-check-fill', 'book', 'gear']
+    else:  # si on a pas l'info sur la guilde
+        menu_selected = ['General', 'Evolution',
+                         'Classement', 'Runes', 'Parametres']
         icons_selected = ["info", 'kanban', 'ladder', 'bag-check-fill', 'gear']
 
 # Menu
 with st.sidebar:
-        selected = option_menu("Menu", menu_selected,
-                            icons=icons_selected, menu_icon='list', default_index=0,
-                            styles={
-            "container": {"padding": "5!important", "background-color": "#03152A"},
-            "icon": {"color": "#0083B9", "font-size": "28px"}, 
-            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#FFFFFF"},
-            "nav-link-selected": {"background-color": "#2C3845"},
-        })
-        
-        if 'pseudo' in st.session_state: # si on a le pseudo du joueur, on l'affiche.
-            # Visibilité
-            list_visibility = ['Non-visible', 'Caché', 'Visible'] # Liste de choix
-            # slider avec, par défaut, la value dans la bdd
-            slider_visibility = st.radio('Visibilité Classement', ['Non-visible', 'Caché', 'Visible à ma guilde', 'Visible à tous'], index=st.session_state.visibility)
-            dict_visibility = {'Non-visible' : 0, 'Caché' : 1, 'Visible à ma guilde' : 2, 'Visible à tous' : 3}
-            # on enregistre si changement
-            requete_perso_bdd('''UPDATE sw_user SET visibility = :visibility where joueur = :joueur''', {'visibility' : dict_visibility[slider_visibility],
-                                                                                                         'joueur' : st.session_state["pseudo"]})
-            st.subheader(f'Joueur : {st.session_state["pseudo"]}')
-            st.subheader(f'Guilde : {st.session_state["guilde"]}') 
-        
+    selected = option_menu("Menu", menu_selected,
+                           icons=icons_selected, menu_icon='list', default_index=0,
+                           styles={
+                               "container": {"padding": "5!important", "background-color": "#03152A"},
+                               "icon": {"color": "#0083B9", "font-size": "28px"},
+                               "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#FFFFFF"},
+                               "nav-link-selected": {"background-color": "#2C3845"},
+                           })
+
+    if 'pseudo' in st.session_state:  # si on a le pseudo du joueur, on l'affiche.
+        # Visibilité
+        list_visibility = ['Non-visible', 'Caché', 'Visible']  # Liste de choix
+        # slider avec, par défaut, la value dans la bdd
+        slider_visibility = st.radio('Visibilité Classement', [
+                                     'Non-visible', 'Caché', 'Visible à ma guilde', 'Visible à tous'], index=st.session_state.visibility)
+        dict_visibility = {'Non-visible': 0, 'Caché': 1,
+                           'Visible à ma guilde': 2, 'Visible à tous': 3}
+        # on enregistre si changement
+        requete_perso_bdd('''UPDATE sw_user SET visibility = :visibility where joueur = :joueur''', {'visibility': dict_visibility[slider_visibility],
+                                                                                                     'joueur': st.session_state["pseudo"]})
+        st.subheader(f'Joueur : {st.session_state["pseudo"]}')
+        st.subheader(f'Guilde : {st.session_state["guilde"]}')
+
 
 # Pages :
 if selected == "Upload JSON":
-    upload_json(category_selected, coef_set, category_selected_spd, coef_set_spd)
-    
+    upload_json(category_selected, coef_set,
+                category_selected_spd, coef_set_spd)
+
 elif selected == 'General':
     general_page()
-    
+
 elif selected == 'Classement':
     classement()
 
 elif selected == 'Bestiaire':
     find_monsters()
-    
+
 elif selected == 'Evolution':
     palier_page()
 
@@ -115,9 +126,10 @@ elif selected == 'Suivi':
 
 elif selected == 'Runes':
     optimisation_rune(category_selected, coef_set)
-    
+
 elif selected == 'Parametres':
     params()
 
 
-st.markdown("<h6 style='text-align: right; color: white;'>by Tomlora </h6>", unsafe_allow_html=True )
+st.markdown("<h6 style='text-align: right; color: white;'>by Tomlora </h6>",
+            unsafe_allow_html=True)
