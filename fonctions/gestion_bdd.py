6f7911b9467d4ff92ae500d4cd230926009c8844
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-12-03 15:13:40 +0100

    Cleaning

--- fonctions/gestion_bdd.py
+++ fonctions/gestion_bdd.py
@@ -12,8 +12,7 @@ DB = os.environ.get('API_SQL')
 engine = create_engine(DB, echo=False)
 
 
-
-def lire_bdd(nom_table, format:str="df", index=None, distinct:bool=False):
+def lire_bdd(nom_table, format: str = "df", index=None, distinct: bool = False):
     """Lire la BDD
 
     Parameters
@@ -24,7 +23,7 @@ def lire_bdd(nom_table, format:str="df", index=None, distinct:bool=False):
             Choix entre 'dict' ou 'df'
     """
     conn = engine.connect()
-    
+
     if distinct:
         requetesql = f'SELECT distinct * FROM {nom_table}'
     else:
@@ -43,7 +42,8 @@ def lire_bdd(nom_table, format:str="df", index=None, distinct:bool=False):
     conn.close()
     return df
 
-def lire_bdd_perso(requests:str, format:str="df", index_col:str="joueur", params=None):
+
+def lire_bdd_perso(requests: str, format: str = "df", index_col: str = "joueur", params=None):
     """Lire la BDD
     Parameters
     -----------
@@ -54,8 +54,8 @@ def lire_bdd_perso(requests:str, format:str="df", index_col:str="joueur", params
     index_col: :class:`str`
             Colonne de l'index de la table
     params : dict avec {'variable' : 'value}
-    
-    
+
+
     Les variables doivent être sous forme %(variable)s
     """
     conn = engine.connect()
@@ -63,14 +63,14 @@ def lire_bdd_perso(requests:str, format:str="df", index_col:str="joueur", params
     if params == None:
         df = pd.read_sql(requests, con=conn, index_col=index_col)
     else:
-        df = pd.read_sql(requests, con=conn, index_col=index_col, params=params)
+        df = pd.read_sql(requests, con=conn,
+                         index_col=index_col, params=params)
 
     df = df.transpose()
     if format == "dict":
         df = df.to_dict()
     conn.close()
     return df
-    
 
 
 def sauvegarde_bdd(df, nom_table, methode_save='replace'):
@@ -86,46 +86,55 @@ def sauvegarde_bdd(df, nom_table, methode_save='replace'):
             Si la table existe déjà, choix entre "append" pour insérer des nouvelles valeurs ou "replace" pour supprimer la table existante et la remplacer
     """
     conn = engine.connect()
-    if not isinstance(df, pd.DataFrame): # si la variable envoyée n'est pas un dataframe, on l'a met au format dataframe
+    # si la variable envoyée n'est pas un dataframe, on l'a met au format dataframe
+    if not isinstance(df, pd.DataFrame):
         df = pd.DataFrame(df)
         df = df.transpose()
-    df.to_sql(nom_table, con=conn, if_exists=methode_save, index=True, method='multi', dtype={'Score' : Float(), 'serie' : BigInteger()})
+    df.to_sql(nom_table, con=conn, if_exists=methode_save, index=True,
+              method='multi', dtype={'Score': Float(), 'serie': BigInteger()})
     conn.close()
-    
+
+
 def supprimer_bdd(nom_table):
     conn = engine.connect()
     sql = text(f'DROP TABLE IF EXISTS {nom_table}')
     conn.execute(sql)
     conn.close()
-    
+
+
 def supprimer_data(joueur, date):
     conn = engine.connect()
-    params_sql = {'joueur' : joueur, 'date' : date}
+    params_sql = {'joueur': joueur, 'date': date}
     sql1 = text(f'''DELETE FROM sw WHERE "id" = :joueur AND date = :date;
                     DELETE FROM sw_score WHERE "id" = :joueur AND date = :date''')  # :var_name
     conn.execute(sql1, params_sql)
 
     conn.close
-    
+
+
 def supprimer_data_all(joueur):
     conn = engine.connect()
-    params_sql = {'joueur' : joueur}
+    params_sql = {'joueur': joueur}
     sql1 = text(f'''DELETE FROM sw WHERE "id" = :joueur;
                     DELETE FROM sw_score WHERE "id" = :joueur;
                     DELETE FROM sw_user WHERE "id" = :joueur;''')  # :var_name
     conn.execute(sql1, params_sql)
 
     conn.close
-    
+
+
 def update_info_compte(joueur, guildeid, compteid):
     conn = engine.connect()
-    params_sql = {'joueur' : joueur, 'guilde_id' : guildeid, 'joueur_id' : compteid}
+    params_sql = {'joueur': joueur,
+                  'guilde_id': guildeid, 'joueur_id': compteid}
     # sql1 = text('UPDATE sw_score SET guilde = :guilde WHERE "Joueur" = :joueur')
-    sql1 = text('UPDATE sw_user SET guilde_id = :guilde_id, joueur = :joueur WHERE joueur_id = :joueur_id;')
+    sql1 = text(
+        'UPDATE sw_user SET guilde_id = :guilde_id, joueur = :joueur WHERE joueur_id = :joueur_id;')
     conn.execute(sql1, params_sql)
     conn.close()
-    
-def get_data_bdd(request:text, dict_params = None):
+
+
+def get_data_bdd(request: text, dict_params=None):
     conn = engine.connect()
     sql = text(request)
     if dict_params == None:
@@ -133,15 +142,16 @@ def get_data_bdd(request:text, dict_params = None):
     else:
         data = conn.execute(sql, dict_params)
     conn.close()
-    
+
     return data
-    
-def requete_perso_bdd(request:text, dict_params:dict):
+
+
+def requete_perso_bdd(request: text, dict_params: dict):
     """
     request : requête sql au format text
-    
+
     dict_params : dictionnaire {variable : valeur}
-    
+
     Rappel
     -------
     Dans la requête sql, une variable = :variable """
@@ -149,18 +159,19 @@ def requete_perso_bdd(request:text, dict_params:dict):
     sql = text(request)
     conn.execute(sql, dict_params)
     conn.close()
-    
-def get_user(joueur, type:str='name_user', id_compte:int=0):
+
+
+def get_user(joueur, type: str = 'name_user', id_compte: int = 0):
     '''Return l'id, la guilde et la visibilité du joueur demandé
     type : name_user ou id'''
     # à adapter avec l'id du compte quand on aura assez d'infos
     conn = engine.connect()
-    if type=='name_user':
+    if type == 'name_user':
         sql = text('SELECT id, guilde_id, visibility , joueur_id, (SELECT guilde from sw_guilde where sw_user.guilde_id = sw_guilde.guilde_id) as guilde FROM sw_user WHERE joueur = :joueur ')
-        data = conn.execute(sql, {'joueur' : joueur})
-    elif type=='id':
+        data = conn.execute(sql, {'joueur': joueur})
+    elif type == 'id':
         sql = text('SELECT id, guilde_id, visibility , joueur_id, (SELECT guilde from sw_guilde where sw_user.guilde_id = sw_guilde.guilde_id) as guilde FROM sw_user WHERE joueur_id =:joueur ')
-        data = conn.execute(sql, {'joueur' : joueur})
+        data = conn.execute(sql, {'joueur': joueur})
     data = data.mappings().all()
     id_joueur = data[0]['id']
 
@@ -168,9 +179,8 @@ def get_user(joueur, type:str='name_user', id_compte:int=0):
     guildeid = data[0]['guilde_id']
     # Dans l'ancien système, on ne prenait pas l'id. On regarde s'il faut maj
     if data[0]['joueur_id'] == 0 and type == 'name_user':
-        sql = text('UPDATE sw_user SET joueur_id = :joueur_id where joueur = :joueur')
-        data = conn.execute(sql, {'joueur_id' : id_compte, 'joueur' : joueur})
-    conn.close()    
+        sql = text(
+            'UPDATE sw_user SET joueur_id = :joueur_id where joueur = :joueur')
+        data = conn.execute(sql, {'joueur_id': id_compte, 'joueur': joueur})
+    conn.close()
     return id_joueur, visibility, guildeid
-    
- 
--- fonctions/visualisation.py
+++ fonctions/visualisation.py
@@ -14,49 +14,53 @@ import pandas as pd
 import streamlit as st
 
 
-def transformation_stats_visu(nom_table, joueur, distinct:bool=False):
+def transformation_stats_visu(nom_table, joueur, distinct: bool = False):
     # Lire la bdd
     df_actuel = lire_bdd(nom_table, distinct=distinct)
     df_actuel = df_actuel.transpose()
     df_actuel.reset_index(inplace=True)
-    
+
     # df_actuel.drop(['id'], axis=1, inplace=True) # inutile
-    
-    #Datetime
+
+    # Datetime
     df_actuel['date'] = pd.to_datetime(df_actuel['date'], format='%d/%m/%Y')
     df_actuel['date'] = df_actuel['date'].dt.strftime('%d/%m/%Y')
-    
+
     # Sort les values
-    
+
     if nom_table == 'sw':
-        df_actuel.sort_values(by=['date','Set'], inplace=True)
+        df_actuel.sort_values(by=['date', 'Set'], inplace=True)
     else:
         df_actuel.sort_values(by='date', inplace=True)
         # df_actuel.drop(['guilde'], axis=1, inplace=True)
     # df_actuel = df_actuel[df_actuel['Joueur'] == joueur]
     df_actuel = df_actuel[df_actuel['id'] == joueur]
     df_actuel.drop(['id', 'index'], axis=1, inplace=True)
-    
+
     # DF final
     if nom_table == 'sw':
-        df_actuel = pd.melt(df_actuel, id_vars=['date', 'Set'], value_vars=['100', '110', '120'], var_name='Palier', value_name='Nombre')
-        df_actuel['datetime'] = pd.to_datetime(df_actuel['date'], format='%d/%m/%Y')
+        df_actuel = pd.melt(df_actuel, id_vars=['date', 'Set'], value_vars=[
+                            '100', '110', '120'], var_name='Palier', value_name='Nombre')
+        df_actuel['datetime'] = pd.to_datetime(
+            df_actuel['date'], format='%d/%m/%Y')
         df_actuel.sort_values(by=['datetime', 'Set', 'Palier'], inplace=True)
         df_actuel.drop(['datetime'], axis=1, inplace=True)
     else:
         # df_actuel = pd.pivot_table(df_actuel, 'score', index='date')
         df_actuel['score'] = df_actuel['score'].astype('int')
-        df_actuel['datetime'] = pd.to_datetime(df_actuel['date'], format='%d/%m/%Y')
+        df_actuel['datetime'] = pd.to_datetime(
+            df_actuel['date'], format='%d/%m/%Y')
         df_actuel.sort_values(by=['datetime'], inplace=True)
         df_actuel.drop(['datetime'], axis=1, inplace=True)
-    
+
     return df_actuel
+
 
 def plotline_evol_rune_visu(df):
     fig = px.line(df, x="date", y="Nombre", color="Set")
     fig.update_layout({
-                'plot_bgcolor': 'rgb(255, 255, 255)',
-                'paper_bgcolor': 'rgba(0, 0, 0,0)'
+        'plot_bgcolor': 'rgb(255, 255, 255)',
+        'paper_bgcolor': 'rgba(0, 0, 0,0)'
     })
     fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='grey')
     fig.update_yaxes(showgrid=False)
@@ -126,7 +130,8 @@ def filter_dataframe(df: pd.DataFrame, key='key') -> pd.DataFrame:
                     ),
                 )
                 if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    user_date_input = tuple(
+                        map(pd.to_datetime, user_date_input))
                     start_date, end_date = user_date_input
                     df = df.loc[df[column].between(start_date, end_date)]
             else:
@@ -134,6 +139,7 @@ def filter_dataframe(df: pd.DataFrame, key='key') -> pd.DataFrame:
                     f"Mot ou partie de mot dans {column}",
                 )
                 if user_text_input:
-                    df = df[df[column].astype(str).str.contains(user_text_input)]
+                    df = df[df[column].astype(
+                        str).str.contains(user_text_input)]
 
     return df
